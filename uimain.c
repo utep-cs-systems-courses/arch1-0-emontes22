b3a2a69a2e23f3862416d84531a2af911bf18dbd
Ervey Montes <ervey025@gmail.com>
2021-02-05 20:37:02 -0700

    Fonts created and Makefile working

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c, 5x7 Font = f, 8x12 Font = e) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -32,6 +32,16 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'f':
+      puts("You selected 5x7 font:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
+      break;
+    case 'e':
+      puts("You selected 8x12 font:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
       break;
     case 'q':
       puts("Bye!");
